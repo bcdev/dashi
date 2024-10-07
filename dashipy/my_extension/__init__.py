fcfd313466ad14fccd44e5bf30ee39e2598bb85a
Norman Fomferra <norman.fomferra@gmail.com>
2024-10-07 14:25:19 +0200

    Simplified sample extension

--- dashipy/my_extension/__init__.py
+++ dashipy/my_extension/__init__.py
@@ -1,3 +1,7 @@
-from .my_extension import ext
+from dashipy.lib.extension import Extension
+from .my_panel_1 import panel as my_panel_1
+from .my_panel_2 import panel as my_panel_2
 
-ext2 = 29
+ext = Extension(__name__)
+ext.add(my_panel_1)
+ext.add(my_panel_2)
