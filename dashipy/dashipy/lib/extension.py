5aa4761b9007233af24bbabded2b2eb81f12e62f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-10-08 15:31:05 +0200

    We now create an initial contribution state from contribution

--- dashipy/dashipy/lib/extension.py
+++ dashipy/dashipy/lib/extension.py
@@ -1,53 +1,6 @@
-from typing import Any, Callable
-from abc import ABC
+from typing import Any
 
-from .callback import Callback, Output, Input
-
-
-class Contribution(ABC):
-    # noinspection PyShadowingBuiltins
-    def __init__(self, name: str):
-        self.name = name
-        self.extension: Extension | None = None
-        self.layout_callback: Callback | None = None
-        self.callbacks: list[Callback] = []
-
-    def to_dict(self) -> dict[str, Any]:
-        d = dict(name=self.name)
-        if self.extension is not None:
-            d.update(extension=self.extension.name)
-        if self.layout_callback is not None:
-            d.update(layout=self.layout_callback.to_dict())
-        if self.callbacks:
-            d.update(callbacks=[cb.to_dict() for cb in self.callbacks])
-        return d
-
-    def layout(self, *args) -> Callable:
-        """Decorator."""
-
-        def decorator(function: Callable) -> Callable:
-            self.layout_callback = Callback.from_decorator(
-                "layout", args, function, outputs_allowed=False
-            )
-            return function
-
-        return decorator
-
-    def callback(self, *args: Input | Output) -> Callable[[Callable], Callable]:
-        """Decorator."""
-
-        def decorator(function: Callable) -> Callable:
-            self.callbacks.append(
-                Callback.from_decorator(
-                    "callback", args, function, outputs_allowed=True
-                )
-            )
-            return function
-
-        return decorator
-
-    def __str__(self):
-        return self.name
+from dashipy.lib.contribution import Contribution
 
 
 class Extension:
@@ -79,7 +32,7 @@ class Extension:
             raise TypeError(
                 f"unrecognized contribution of type {contrib_type.__qualname__}"
             )
-        contribution.extension = self
+        contribution.extension = self.name
         contributions: list[Contribution] = getattr(self, contrib_point_name)
         contributions.append(contribution)
 
