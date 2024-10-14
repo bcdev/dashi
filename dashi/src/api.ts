30f1de8deb162e9f99003a192883e118eba9aa77
Norman Fomferra <norman.fomferra@gmail.com>
2024-10-14 14:42:41 +0200

    turned serverUrl and endpointName into options

--- dashi/src/api.ts
+++ dashi/src/api.ts
@@ -3,18 +3,25 @@ import { ContribPoint, Contributions } from "./model/extension";
 import { CallbackRequest, StateChangeRequest } from "./model/callback";
 import { callApi } from "./utils/fetchApiResult";
 
-const serverUrl = "http://localhost:8888";
+const defaultServerUrl = "http://localhost:8888";
+const defaultEndpointName = "dashi";
 
-export async function fetchContributions(): Promise<Contributions> {
-  return callApi(`${serverUrl}/dashi/contributions`);
+export interface ApiOptions {
+  serverUrl?: string;
+  endpointName?: string;
+}
+
+export async function fetchContributions(options?: ApiOptions): Promise<Contributions> {
+  return callApi(makeUrl("contributions", options));
 }
 
 export async function fetchInitialComponentState(
   contribPoint: ContribPoint,
   contribIndex: number,
   inputValues: unknown[],
+  options?: ApiOptions
 ): Promise<ComponentState> {
-  return callApi(`${serverUrl}/dashi/layout/${contribPoint}/${contribIndex}`, {
+  return callApi(makeUrl(`layout/${contribPoint}/${contribIndex}`, options), {
     body: JSON.stringify({ inputValues }),
     method: "post",
   });
@@ -22,9 +29,16 @@ export async function fetchInitialComponentState(
 
 export async function fetchStateChangeRequests(
   callbackRequests: CallbackRequest[],
+  options?: ApiOptions
 ): Promise<StateChangeRequest[]> {
-  return callApi(`${serverUrl}/dashi/callback`, {
+  return callApi(makeUrl("callback", options), {
     body: JSON.stringify({ callbackRequests: callbackRequests }),
     method: "post",
   });
+}
+
+function makeUrl(path: string, options?: ApiOptions): string {
+  const serverUrl = options?.serverUrl || defaultServerUrl;
+  const endpointName = options?.endpointName || defaultEndpointName;
+  return `${serverUrl}/${endpointName}/${path}`
 }
