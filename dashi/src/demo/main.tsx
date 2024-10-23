4d88ca360f73261303ea590d977064af946f95d5
b-yogesh <yogesh09121995@gmail.com>
2024-10-23 09:00:20 +0200

    Upgrade React to v18 (#18)
    * Upgrade to react 18
    
    * Update package-lock.json
    
    * Update dashi/src/lib/store.ts
    
    Co-authored-by: Norman Fomferra <norman.fomferra@brockmann-consult.de>
    
    ---------
    
    Co-authored-by: Norman Fomferra <norman.fomferra@brockmann-consult.de>

--- dashi/src/demo/main.tsx
+++ dashi/src/demo/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
@@ -9,9 +9,8 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")!,
 );
