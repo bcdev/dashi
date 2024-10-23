b8b39178ca63b796e310dc8c623c41b6c8bcc916
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-10-23 17:40:27 +0200

    removed plotly

--- dashi/vite.config.ts
+++ dashi/vite.config.ts
@@ -32,11 +32,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
-      // Alias 'plotly.js' to use the minified dist version instead of the source code.
-      "plotly.js": resolve(
-        __dirname,
-        "node_modules/plotly.js/dist/plotly.min.js",
-      ),
     },
   },
   publicDir: false,
@@ -60,7 +55,7 @@ export default defineConfig({
           microdiff: "diff",
           react: "React",
           "react-dom": "ReactDOM",
-          "react-plotly.js": "Plot",
+          "react-vega": "ReactVega",
           zustand: "zustand",
         },
       },
