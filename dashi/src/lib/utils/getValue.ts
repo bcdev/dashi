ba1eead1a138870466e8b2e41378704dfeebb5e1
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-11-05 19:41:42 +0100

    New type PropertyPath

--- dashi/src/lib/utils/getValue.ts
+++ dashi/src/lib/utils/getValue.ts
@@ -1,4 +1,6 @@
-export function getValue(obj: object, path: (string | number)[]): unknown {
+export type PropertyPath = (string | number)[];
+
+export function getValue(obj: object, path: PropertyPath): unknown {
   let value: unknown = obj;
   for (const key of path) {
     if (typeof value === "object") {
