a48312e56b16d67ea62a658e1836506a17bbee43
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-11-11 07:28:50 +0100

    new helper

---
+++ dashi/src/lib/utils/mapObject.ts
@@ -0,0 +1,14 @@
+export function mapObject<
+  S extends Record<string, V1>,
+  V1 = unknown,
+  V2 = unknown,
+>(
+  obj: S,
+  transformValue: (value: V1, key: string | number) => V2,
+): Record<string, V2> {
+  const obj2: { [k: string]: V2 } = {};
+  Object.getOwnPropertyNames(obj).forEach((key) => {
+    obj2[key] = transformValue(obj[key], key);
+  });
+  return obj2;
+}
