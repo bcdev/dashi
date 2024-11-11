0edcecff23b608e486227d13b23e3eed8fc216a5
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-11-11 16:08:50 +0100

    prefix logging output

--- dashi/src/lib/actions/helpers/invokeCallbacks.ts
+++ dashi/src/lib/actions/helpers/invokeCallbacks.ts
@@ -4,20 +4,27 @@ import { fetchCallback } from "@/lib/api/fetchCallback";
 import { applyStateChangeRequests } from "@/lib/actions/helpers/applyStateChangeRequests";
 
 export function invokeCallbacks(callbackRequests: CallbackRequest[]) {
+  const { configuration } = store.getState();
+  const shouldLog = configuration.logging?.enabled;
   if (!callbackRequests.length) {
+    if (shouldLog) {
+      console.info(`dashi: invokeCallbacks - no requests`, callbackRequests);
+    }
     return;
   }
-  const { configuration } = store.getState();
   const invocationId = getInvocationId();
-  if (import.meta.env.DEV) {
-    console.debug(`invokeCallbacks (${invocationId})-->`, callbackRequests);
+  if (shouldLog) {
+    console.info(
+      `dashi: invokeCallbacks (${invocationId})-->`,
+      callbackRequests,
+    );
   }
   fetchCallback(callbackRequests, configuration.api).then(
     (changeRequestsResult) => {
       if (changeRequestsResult.data) {
-        if (import.meta.env.DEV) {
-          console.debug(
-            `invokeCallbacks <--(${invocationId})`,
+        if (shouldLog) {
+          console.info(
+            `dashi: invokeCallbacks <--(${invocationId})`,
             changeRequestsResult.data,
           );
         }
