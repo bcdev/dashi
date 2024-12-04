1d4eba7d434e51b3343def8513774b07219a0755
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-12-04 20:37:36 +0100

    relative plugin imports

--- chartlets.js/src/lib/plugins/mui/index.ts
+++ chartlets.js/src/lib/plugins/mui/index.ts
@@ -1,23 +1,13 @@
 import { componentRegistry as cr } from "@/lib";
 
 export default function initPlugin() {
-  import("@/lib/plugins/mui/Box").then((m) => cr.register("Box", m.Box));
-  import("@/lib/plugins/mui/Button").then((m) =>
-    cr.register("Button", m.Button),
+  import("./Box").then((m) => cr.register("Box", m.Box));
+  import("./Button").then((m) => cr.register("Button", m.Button));
+  import("./Checkbox").then((m) => cr.register("Checkbox", m.Checkbox));
+  import("./CircularProgress").then((m) =>
+    cr.register("CircularProgress.ts", m.CircularProgress),
   );
-  import("@/lib/plugins/mui/Checkbox").then((m) =>
-    cr.register("Checkbox", m.Checkbox),
-  );
-  import("@/lib/plugins/mui/CircularProgress").then((m) =>
-    cr.register("CircularProgress", m.CircularProgress),
-  );
-  import("@/lib/plugins/mui/IconButton").then((m) =>
-    cr.register("IconButton", m.IconButton),
-  );
-  import("@/lib/plugins/mui/Select").then((m) =>
-    cr.register("Select", m.Select),
-  );
-  import("@/lib/plugins/mui/Typography").then((m) =>
-    cr.register("Typography", m.Typography),
-  );
+  import("./IconButton").then((m) => cr.register("IconButton", m.IconButton));
+  import("./Select").then((m) => cr.register("Select", m.Select));
+  import("./Typography").then((m) => cr.register("Typography", m.Typography));
 }
--- chartlets.js/src/lib/plugins/vega/index.ts
+++ chartlets.js/src/lib/plugins/vega/index.ts
@@ -1,7 +1,5 @@
 import { componentRegistry as cr } from "@/lib";
 
 export default function initPlugin() {
-  import("@/lib/plugins/vega/VegaChart").then((m) =>
-    cr.register("VegaChart", m.VegaChart),
-  );
+  import("./VegaChart").then((m) => cr.register("VegaChart", m.VegaChart));
 }
