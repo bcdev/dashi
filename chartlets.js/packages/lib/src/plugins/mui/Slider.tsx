a912fe36de0f905edbf820de060555dcc1baae1b
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2024-12-16 15:34:31 +0100

    Update Slider.tsx [test for CI]

--- chartlets.js/packages/lib/src/plugins/mui/Slider.tsx
+++ chartlets.js/packages/lib/src/plugins/mui/Slider.tsx
@@ -1,23 +1,25 @@
-import type { ElementType } from "react";
 import MuiSlider from "@mui/material/Slider";
 
 import type { ComponentProps, ComponentState } from "@/index";
 
-interface SliderState extends ComponentState {
-  component?: ElementType<Element>;
-}
+interface SliderState extends ComponentState {}
 
 interface SliderProps extends ComponentProps, SliderState {
-  defaultValue: number;
+  defaultValue?: number;
 }
 
-export const Slider = ({ id, style, defaultValue, component }: SliderProps) => {
+export const Slider = ({
+  id,
+  style,
+  defaultValue,
+  // onChange,
+}: SliderProps) => {
   return (
     <MuiSlider
       id={id}
       style={style}
       defaultValue={defaultValue}
-      component={component || "div"}
-    ></MuiSlider>
+      // onChange={onChange}
+    />
   );
 };
