9678716d7347bbef23b2f9cbde5c5d36173c2e4e
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-12-18 18:22:06 +0100

    using `Literal` typing form

--- chartlets.py/chartlets/components/divider.py
+++ chartlets.py/chartlets/components/divider.py
@@ -1,26 +1,28 @@
 from dataclasses import dataclass
+from typing import Literal
 
 from chartlets import Container
 
 
 @dataclass(frozen=True)
 class Divider(Container):
-    """The Divider component provides a thin,
+    """The `Divider` component provides a thin,
     unobtrusive line for grouping elements to reinforce visual hierarchy.
     """
 
-    orientation: str | None = None
+    orientation: Literal["horizontal", "vertical"] | None = None
     """The orientation. Can be `horizontal` (default) or `vertical`."""
 
-    variant: str | None = None
+    variant: Literal["fullWidth", "inset", "middle"] | None = None
     """The variant. One of `fullWidth ` (default), `inset`, and `middle`."""
+
+    textAlign: Literal["left", "center", "right"] | None = None
+    """Use the `textAlign` prop to align elements that are 
+    wrapped by the divider. One of `left`, `center` (default), and `right`.
+    """
 
     flexItem: bool | None = None
     """Use the `flexItem` prop to display the divider when it's being 
     used in a flex container.
     """
 
-    textAlign: str | None = None
-    """Use the `textAlign` prop to align elements that are 
-    wrapped by the divider. One of `center` (default), `left`, and `right`.
-    """
